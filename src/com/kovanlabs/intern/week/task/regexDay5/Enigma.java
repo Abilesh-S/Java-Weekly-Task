e52609410d8d2d1d101d45f4e4749488f1b8a02a
Abilesh S <abilesh1067@gmail.com>
2026-02-02 10:19:42 +0530

    Changes in Week 1 Task1

--- src/com/kovanlabs/intern/week/task/regexDay5/Enigma.java
+++ src/com/kovanlabs/intern/week/task/regexDay5/Enigma.java
@@ -2,30 +2,34 @@ package com.kovanlabs.intern.week.task.regexDay5;
 
 public class Enigma {
 
-    private char[] encrypt(String s , int key ){
-        char[] encryptMsg = new char[s.length()];
+    private String xorOperation(String s , int key){
+        char[] xorArray = new char[s.length()];
         for(int i = 0; i < s.length(); i++){
-            encryptMsg[i] = (char) (s.charAt(i) ^ key);
+            xorArray[i] = (char) (s.charAt(i) ^ key);
         }
+        String xorString = new String(xorArray);
+        return xorString;
+    }
+
+    private String encrypt(String s , int key ){
+        String encryptMsg = new String();
+        encryptMsg = xorOperation(s , key);
         return encryptMsg;
     }
 
-    private StringBuffer decrypt(char[] msg , int key ){
-        StringBuffer decryptMsg = new StringBuffer();
-        for(int i = 0; i < msg.length; i++){
-            char temp = (char)(msg[i] ^ key);
-            decryptMsg = decryptMsg.append(temp);
-        }
+    private String decrypt(String msg , int key ){
+        String decryptMsg = new String();
+        decryptMsg = xorOperation(msg, key);
         return decryptMsg;
     }
 
     public static void main(String[] args) {
-        String s = "DUDE";
+        String s = "ABILESH ROCKSTAR";
         Enigma e = new Enigma();
         int key = 7;
-        char[] encryptMsg = e.encrypt(s , key);
+        String encryptMsg = e.encrypt(s , key);
         System.out.println(encryptMsg);
-        StringBuffer decryptMsg = e.decrypt(encryptMsg , key);
+        String decryptMsg = e.decrypt(encryptMsg , key);
         System.out.println(decryptMsg);
 
     }
