7017b52f795b9c20c935781784457e6c2b0bf6e0
Abilesh S <abilesh1067@gmail.com>
2026-02-28 14:27:38 +0530

    Day 18 Task Completed

--- src/main/java/com/kovanlabs/intern/week/task/enumday9/BattleArena.java
+++ src/main/java/com/kovanlabs/intern/week/task/enumday9/BattleArena.java
@@ -1,4 +1,4 @@
-package com.kovanlabs.intern.week.task.enumday9;
+package main.java.com.kovanlabs.intern.week.task.enumday9;
 
 abstract class Characther {
     protected int health;
--- src/test/java/com/kovanlabs/intern/week/task/topicspractice/ListIteratorDemoTest.java
+++
@@ -1,6 +0,0 @@
-package com.kovanlabs.intern.week.task.topicspractice;
-
-import static org.junit.jupiter.api.Assertions.*;
-class ListIteratorDemoTest {
-  
-}
